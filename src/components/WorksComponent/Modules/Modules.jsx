ea39e323f7dc0c9192b762921de888c9e970cad9
Jason <jbjbro6@gmail.com>
2019-03-02 03:48:59 -0600

    Image state to props

--- src/components/WorksComponent/Modules/Modules.jsx
+++ src/components/WorksComponent/Modules/Modules.jsx
@@ -3,67 +3,68 @@ import React from 'react';
 import media from 'styled-media-query';
 import styled from 'styled-components';
 
-const Outer = styled.div`
-  background: white;
-  border-radius: 15px;
-  padding: 10px;
-  margin-left: 25px;
-  text-decoration: none;
-  justify-content: center;
-  width: 32em;
-  ${media.lessThan('medium')`
+class Modules extends React.Component {
+  render () {
+    const Outer = styled.div`
+      background: white;
+      border-radius: 15px;
+      padding: 10px;
+      margin-left: 25px;
+      text-decoration: none;
+      justify-content: center;
+      width: 32em;
+      ${media.lessThan('medium')`
     height: 100%;
     padding: 10px;
     margin-bottom: 50px;
     text-decoration: none;
     width: 100%;
   `}
-`;
+    `;
 
-const Link = styled.a`
-  color: black;
-  text-decoration: none;
-  &:hover {
-    text-decoration: none;
-  }
-`;
+    const Link = styled.a`
+      color: black;
+      text-decoration: none;
+      &:hover {
+        text-decoration: none;
+      }
+    `;
 
-const Paragraph = styled.p`
-  color: black;
-  text-decoration: none;
-`;
+    const Paragraph = styled.p`
+      color: black;
+      text-decoration: none;
+    `;
 
-const Image = styled.img`
-  width: 100%;
-  ${media.lessThan('medium')`
+    const Image = styled.img`
+      width: 100%;
+      ${media.lessThan('medium')`
     width: 100%;
   `}
-`;
+    `;
 
-const Header = styled.h4`
-  align: center;
-`;
-
-const Modules = props => {
-  return (
-    <Outer>
-      <Link href={`${props.link}`}>
-        <div>
-          <Header>
-            <i class={props.icon} />
-            &nbsp;&nbsp;
-            {props.headerText}
-          </Header>
-          <Image src={`${props.img}`} alt='' />
-          <Paragraph>
-            {props.pBefore}
-            <br />
-            {props.pAfter}
-          </Paragraph>
-        </div>
-      </Link>
-    </Outer>
-  );
-};
+    const Header = styled.h4`
+      align: center;
+    `;
+    return (
+      <Outer>
+        <Link href={`${this.props.link}`}>
+          <div>
+            <Header>
+              <i class={this.props.icon} />
+              &nbsp;&nbsp;
+              {this.props.headerText}
+            </Header>
+            <Image src={`${this.props.img}`} alt="" />
+            <Paragraph>
+              {this.props.pBefore}
+              <br />
+              {this.props.pAfter}
+            </Paragraph>
+          </div>
+        </Link>
+      </Outer>
+    );
+  }
+}
 
 export default Modules;
