16266a7cd1fb94240688989e61e0353467b98cee
Jason <jbjbro6@gmail.com>
2019-03-01 20:55:25 -0600

    Moved Header

--- src/components/HomeComponent/HomeComponent.jsx
+++ src/components/HomeComponent/HomeComponent.jsx
@@ -24,45 +24,11 @@ const OuterContent = styled.div`
   padding: 1em;
 `;
 
-const AboutMe = styled.div`
-  width: 100%;
-  height: 100px;
-  display: flex;
-  justify-content: center;
-  ${media.lessThan('medium')`
-    flex-direction: column;
-    justify-content: center;
-    height: 100%;
-    width: 100%;
-  `}
-`;
-
-const Header = styled.h2`
-  display: flex;
-  text-shadow: 5px 0 5px  black;
-  font-size: 85px;
-  display: flex;
-  justify-content: center;
-  color: white;
-  ${media.lessThan('medium')`
-    display: flex;
-    flex-direction: column;
-    text-align: center;
-    text-shadow: 5px 0 5px  black;
-    font-size: 55px;
-    height: 100%;
-    width: 100%;
-  `}
-`;
-
 class HomeComponent extends Component {
   render () {
     return (
       <Outer>
         <OuterContent>
-          <AboutMe>
-            <Header>Jason Brown</Header>
-          </AboutMe>
           <BioContent />
         </OuterContent>
       </Outer>
