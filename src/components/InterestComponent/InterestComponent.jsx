5a2454d2ee67c189a6283467aabb1dd17ec96dd0
Jason <jbjbro6@gmail.com>
2019-03-04 22:22:50 -0600

    Vid LFS

--- src/components/InterestComponent/InterestComponent.jsx
+++ src/components/InterestComponent/InterestComponent.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import styled, { keyframes } from 'styled-components';
 import media from 'styled-media-query';
+import ReactPlayer from 'react-player';
+import vid from '../../Videos/BOPs.mp4';
 
 class InterestComponent extends Component {
   render () {
@@ -8,6 +10,14 @@ class InterestComponent extends Component {
       position: relative;
       padding-top: 56.25%; /* 720 / 1280 = 0.5625 */
       background: white;
+    `;
+
+    const ReactPlay = styled(ReactPlayer)`
+      position: absolute;
+      margin-top: 44px;
+      top: 0;
+      left: 0;
+      z-index: 10;
     `;
 
     // const InnerContaitner = styled.div`
@@ -40,7 +50,16 @@ class InterestComponent extends Component {
     //   `}
     // `;
     return (
-      <Container />
+      <Container>
+        <ReactPlay
+          url={vid}
+          youtubeConfig={{ playerVars: { showinfo: 1 } }}
+          width='100%'
+          height='100%'
+          playing
+          loop='true'
+        />
+      </Container>
     );
   }
 }
