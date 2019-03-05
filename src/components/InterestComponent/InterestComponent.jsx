b7c6e620b5d08e77656376ca18992e467d2b00cf
Jason <jbjbro6@gmail.com>
2019-03-04 22:26:59 -0600

    remove video

--- src/components/InterestComponent/InterestComponent.jsx
+++ src/components/InterestComponent/InterestComponent.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import styled, { keyframes } from 'styled-components';
 import media from 'styled-media-query';
 import ReactPlayer from 'react-player';
-import vid from '../../Videos/BOPs.mp4';
 
 class InterestComponent extends Component {
   render () {
@@ -10,14 +9,6 @@ class InterestComponent extends Component {
       position: relative;
       padding-top: 56.25%; /* 720 / 1280 = 0.5625 */
       background: white;
-    `;
-
-    const ReactPlay = styled(ReactPlayer)`
-      position: absolute;
-      margin-top: 44px;
-      top: 0;
-      left: 0;
-      z-index: 10;
     `;
 
     // const InnerContaitner = styled.div`
@@ -50,16 +41,7 @@ class InterestComponent extends Component {
     //   `}
     // `;
     return (
-      <Container>
-        <ReactPlay
-          url={vid}
-          youtubeConfig={{ playerVars: { showinfo: 1 } }}
-          width='100%'
-          height='100%'
-          playing
-          loop='true'
-        />
-      </Container>
+      <Container />
     );
   }
 }
