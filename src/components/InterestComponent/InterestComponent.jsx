9df95652869045f3f049d4d2dd6ed9caf9a8199f
Jason <jbjbro6@gmail.com>
2019-03-04 22:13:54 -0600

    Update InterestComponent.jsx

--- src/components/InterestComponent/InterestComponent.jsx
+++ src/components/InterestComponent/InterestComponent.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import styled, { keyframes } from 'styled-components';
 import media from 'styled-media-query';
-import ReactPlayer from 'react-player';
-import vid from '../../BOPs.mp4';
 
 class InterestComponent extends Component {
   render () {
@@ -10,14 +8,6 @@ class InterestComponent extends Component {
       position: relative;
       padding-top: 56.25%; /* 720 / 1280 = 0.5625 */
       background: white;
-    `;
-
-    const ReactPlay = styled(ReactPlayer)`
-      position: absolute;
-      margin-top: 44px;
-      top: 0;
-      left: 0;
-      z-index: 10;
     `;
 
     // const InnerContaitner = styled.div`
@@ -50,16 +40,7 @@ class InterestComponent extends Component {
     //   `}
     // `;
     return (
-      <Container>
-        <ReactPlay
-          url={vid}
-          youtubeConfig={{ playerVars: { showinfo: 1 } }}
-          width='100%'
-          height='100%'
-          playing
-          loop='true'
-        />
-      </Container>
+      <Container />
     );
   }
 }
